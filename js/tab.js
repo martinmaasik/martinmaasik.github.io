cb8dec48c754c3f08bf3dedf04159698f806f7d8
Martin Maasik <martinmaasik@gmail.com>
2020-12-30 00:45:12 +0200

    e

--- js/tab.js
+++ js/tab.js
@@ -1,12 +1,10 @@
-$(document).ready(function() {
-    var url = 'http://martinmaasik.github.io/system.json';
-    var j = [];
-    $.ajax({
-      type: 'GET',
-      url: url,
-      dataType: 'json',
-      success: function(data) { j = data;},
-      async: false
-    });
-    alert(j.Users[0].Name);
+var url = 'http://martinmaasik.github.io/system.json';
+var j = [];
+$.ajax({
+  type: 'GET',
+  url: url,
+  dataType: 'json',
+  success: function(data) { j = data;},
+  async: false
 });
+alert(j.Users[0].Name);
